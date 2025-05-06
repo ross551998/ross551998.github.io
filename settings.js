95b4cd9be2294c00f09fb168f297ea33322cea6d
ross <rsims1601@hotmail.com>
2025-05-06 16:05:59 -0500

    m- commit added version control for user

--- settings.js
+++ settings.js
@@ -14,3 +14,33 @@ document.getElementById('iconchange').addEventListener('click', function() {
     newsContext.style.animation = 'slideInOut 3s';
     newsContext.innerText = 'changed icon';
 })
+//version control for users
+const  currentVersion = "3.0.0";
+const versionUrl = '/version.json'; // URL to fetch the version data
+async function shouldCheckForUpdates() {
+    const lastCheck = localStorage.getItem('lastVersionCheck');
+    const today = new Date().toISOString().split('T')[0];
+    if(lastCheck !== today) {
+        localStorage.setItem('lastVersionCheck', today);
+        return true;
+    }
+    return false;
+}
+async function CheckForUpdate() {
+    if(!shouldCheckForUpdates()) return;
+    try {
+        const response = await fetch(versionUrl, {cache: 'no-cache'});
+        const data = await response.json();
+        if(data.version !== currentVersion) {
+            alert("new update available, please save your progress and refresh the page ");
+        }
+    } catch(error) {
+        console.error('Error fetching version data:', error);
+        alert('Error fetching version data. Please try again later.');
+    }
+}
+window.addEventListener('load', () => {
+    CheckForUpdate();
+})
+console.log('checking for updates...');
+console.log('current version:', currentVersion);
